d3b1e9cd1d299c3e690eadaefc6ac5cc24ffdc3c
mahesh <maheshkhatiwada17@gmail.com>
2021-02-27 23:15:39 +0545

    added result component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Result.css";
 
 function Result() {
+    const [image,setImage]=useState(null);
+
+    const inputHandler=(e)=>{
+        setImage( URL.createObjectURL(e.target.files[0]));
+    }
+
+
+    
     return (
         <div className="result">
             <h1>Result</h1>
+                <input  type="file" className="custom-file-input" onChange={inputHandler}/>
+            <div className="result__image">
+                <img src={image} alt=""/>
+            </div>
+            <div className="result__answer">
+               <p>Accuracy : 99%</p>
+               <p>Type:  Epidural Haemorrhage </p>
+            </div>
         </div>
     )
 }
